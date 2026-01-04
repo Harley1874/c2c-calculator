bcc0635919e4365560fb73f668b9080ae642af3b
陆凌风 <lulingfengweb@gmail.com>
2026-01-04 15:35:20 +0800

    add loger

--- apps/api/src/c2c/c2c.controller.ts
+++ apps/api/src/c2c/c2c.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Logger } from '@nestjs/common';
 import { C2CService } from './c2c.service';
 
 @Controller('c2c')
 export class C2CController {
+  private readonly logger = new Logger(C2CController.name);
+
   constructor(private readonly c2cService: C2CService) {}
 
   @Get('price')
@@ -11,6 +13,7 @@ export class C2CController {
     @Query('fiat') fiat: string = 'CNY',
     @Query('tradeType') tradeType: string = 'SELL',
   ) {
+    this.logger.log(`Received getPrice request for ${asset}/${fiat} ${tradeType}`);
     return this.c2cService.getPrice(asset, fiat, tradeType);
   }
 
@@ -20,6 +23,7 @@ export class C2CController {
     @Query('fiat') fiat: string = 'CNY',
     @Query('tradeType') tradeType: string = 'SELL',
   ) {
+    this.logger.log(`Received force-refresh request for ${asset}/${fiat} ${tradeType}`);
     return this.c2cService.forceRefresh(asset, fiat, tradeType);
   }
 }
